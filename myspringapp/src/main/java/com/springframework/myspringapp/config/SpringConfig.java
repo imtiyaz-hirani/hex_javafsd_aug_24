28e38fa3a9b6e2a9faac423f73452a0e76be98a5
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-09-02 11:36:54 +0530

    spring-app

---
+++ myspringapp/src/main/java/com/springframework/myspringapp/config/SpringConfig.java
@@ -0,0 +1,28 @@
+package com.springframework.myspringapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration  //- this will ensure that the class gets auto detected during app stratup 
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer{
+
+	@Bean //<-- registering this object of InternalResourceViewResolver with Spring
+	InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/META-INF/resources/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**")
+		.addResourceLocations("/resources/");
+	}
+}
---
+++ myspringapp/src/main/java/com/springframework/myspringapp/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.springframework.myspringapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //<-- this tells spring that it is a controller
+public class MainController {
+
+	@GetMapping("/")
+	public String showHome() {
+		return "home";
+	}
+	
+}
