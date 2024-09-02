04ca723168525c5211cc8fe40d31b0c163adf8a7
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-09-02 12:31:53 +0530

    spring-app-config-added

--- myspringapp/src/main/java/com/springframework/myspringapp/config/SpringConfig.java
+++ myspringapp/src/main/java/com/springframework/myspringapp/config/SpringConfig.java
@@ -1,6 +1,7 @@
 package com.springframework.myspringapp.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -9,12 +10,13 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration  //- this will ensure that the class gets auto detected during app stratup 
 @EnableWebMvc
+@ComponentScan(basePackages = "com.springframework.myspringapp")
 public class SpringConfig implements WebMvcConfigurer{
 
 	@Bean //<-- registering this object of InternalResourceViewResolver with Spring
 	InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setPrefix("/META-INF/resources/WEB-INF/views/");
+		viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
--- myspringapp/src/main/java/com/springframework/myspringapp/controller/MainController.java
+++ myspringapp/src/main/java/com/springframework/myspringapp/controller/MainController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller //<-- this tells spring that it is a controller
 public class MainController {
 
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public String showHome() {
-		return "home";
+	return "home";
 	}
 	
 }
