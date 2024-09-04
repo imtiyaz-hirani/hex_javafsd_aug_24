f1c82a331f4131066f6a42fa365108161947cb08
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-09-04 12:00:49 +0530

    mocking-added

--- myspringapp/src/test/java/com/springframework/myspringapp/MainServiceTest.java
+++ myspringapp/src/test/java/com/springframework/myspringapp/MainServiceTest.java
@@ -2,25 +2,38 @@ package com.springframework.myspringapp;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.springframework.myspringapp.model.Product;
+import com.springframework.myspringapp.repository.ProductRepository;
 import com.springframework.myspringapp.service.MainService;
 
 @SpringBootTest
 public class MainServiceTest {
 
-	@Autowired
+	@InjectMocks
 	private MainService mainService;
 	
+	@Mock
+	private ProductRepository productRepository;
+	 
 	@Test
 	public void getProductTitleTest() {
+		List<String> mockList = Arrays.asList("A","B","C","D","E","F","I");
+		
+		when(mainService.getProductTitle()).thenReturn(mockList);
+		
 		int exceptedNum = 7; 
 		int actualNum = mainService.getProductTitle().size();
 		assertEquals(exceptedNum, actualNum);
@@ -28,11 +41,35 @@ public class MainServiceTest {
 	
 	@Test
 	public void getAllProductTest() {
+		Product p1 = new Product();
+		p1.setId(1);
+		p1.setTitle("title1");
+		p1.setPrice(100);
+		
+		Product p2 = new Product();
+		p2.setId(4);
+		p2.setTitle("title2");
+		p2.setPrice(120);
+		
+		Product p3 = new Product();
+		p3.setId(2);
+		p3.setTitle("title3");
+		p3.setPrice(200);
+		
+		List<Product> list = Arrays.asList(p1,p2,p3);
+		
+		List<Product> list1 = Arrays.asList(p1);
+		List<Product> list2 = Arrays.asList();
+		
+		when(mainService.getAllProduct("active")).thenReturn(list);
+		when(mainService.getAllProduct("inactive")).thenReturn(list1);
+		when(mainService.getAllProduct("")).thenReturn(list2);
+		
 		List<Product> listActive =  mainService.getAllProduct("active"); //use case 1 
 		List<Product> listInActive =  mainService.getAllProduct("inactive"); //use case 2
 		List<Product> listInvalid =  mainService.getAllProduct(""); //use case 3
 		
-		int expectedActive = 6; 
+		int expectedActive = 3; 
 		int expectedInactive = 1;  
 		int expectedInvalid = 0; 
 		
@@ -58,7 +95,7 @@ public class MainServiceTest {
 		
 	}
 	
-	@Test
+ 
 	public void filterProductListTest() {
 		Product p1 = new Product();
 		p1.setId(1);
