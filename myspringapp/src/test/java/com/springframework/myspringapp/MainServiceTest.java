09347f7f46d22e811f99cafdd1b5d1d7d81d14da
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-09-04 10:11:47 +0530

    get-products-active-inactive-test

--- myspringapp/src/test/java/com/springframework/myspringapp/MainServiceTest.java
+++ myspringapp/src/test/java/com/springframework/myspringapp/MainServiceTest.java
@@ -1,11 +1,15 @@
 package com.springframework.myspringapp;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.springframework.myspringapp.model.Product;
 import com.springframework.myspringapp.service.MainService;
 
 @SpringBootTest
@@ -20,4 +24,32 @@ public class MainServiceTest {
 		int actualNum = mainService.getProductTitle().size();
 		assertEquals(exceptedNum, actualNum);
 	}
+	
+	@Test
+	public void getAllProductTest() {
+		List<Product> listActive =  mainService.getAllProduct("active"); //use case 1 
+		List<Product> listInActive =  mainService.getAllProduct("inactive"); //use case 2
+		List<Product> listInvalid =  mainService.getAllProduct(""); //use case 3
+		
+		int expectedActive = 6; 
+		int expectedInactive = 1;  
+		int expectedInvalid = 0; 
+		
+		assertNotNull(listActive);
+		assertNotNull(listInActive);
+		assertNotNull(listInvalid);
+		
+		int actualActive = listActive.size();
+		int actualInActive = listInActive.size();
+		int actualInvalid = listInvalid.size();
+		
+		assertEquals(expectedActive, actualActive);
+		assertEquals(expectedInactive, actualInActive);
+		assertEquals(expectedInvalid, actualInvalid);
+		
+	}
 }
+
+
+
+
