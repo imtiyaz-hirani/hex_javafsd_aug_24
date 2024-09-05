cac195778119d5d13afdd57e8e3fdca1fe3f8eaa
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-09-05 11:18:20 +0530

    create-tables-from-entity-classes

--- myhibernateapp/src/com/hibernate/main/App.java
+++ myhibernateapp/src/com/hibernate/main/App.java
@@ -1,8 +1,11 @@
 package com.hibernate.main;
 
+import java.util.Scanner;
+
 import org.hibernate.SessionFactory;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 //JPA: jakarta Persistence API 
@@ -17,11 +20,35 @@ public class App {
 		 * we use Persistence class and EntityManagerFactory interface and connect to persistence unit (ecomUnit)
 		 */
 		
-		SessionFactory sessionFactory = (SessionFactory) Persistence.createEntityManagerFactory("ecomUnit");
+		SessionFactory sessionFactory = (SessionFactory) Persistence.createEntityManagerFactory("booking_platform");
 		
 		/*Set up EntityManager*/
 		EntityManager entityManager = sessionFactory.createEntityManager();
-		System.out.println(entityManager);
+		 
+		/*Set up EntityTransaction */
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("--------------Main Menu: ECOM--------------");
+			System.out.println("1. Insert");
+			System.out.println("0. Exit");
+			int input = sc.nextInt();
+			if(input == 0 ) {
+				System.out.println("Exiting, thank you!!!");
+				break; //breaks the while loop 
+			}
+			
+			switch(input) {
+				case 1: 
+					break;
+				default: 
+					break; 
+			}
+		}
+		
+		sc.close();
 	}
 
 }
---
+++ myhibernateapp/src/com/hibernate/main/model/Customer.java
@@ -0,0 +1,56 @@
+package com.hibernate.main.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity //<-- create the table in DB 
+public class Customer {
+
+	@Id //this makes id a Primary key 
+	@GeneratedValue(strategy = GenerationType.AUTO)  //this will make id auto_incremental 
+	private int id; 
+	
+	private String name; 
+	
+	private String contact; 
+	
+	@OneToOne
+	private User user; //user_id will be created as Foreign Key in customer table
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	} 
+	
+	
+}
