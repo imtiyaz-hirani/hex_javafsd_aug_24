664ed92235bac02c0fd45116a7e7ee9dee53876b
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-09-09 11:55:13 +0530

    customer-getall

--- my-boot-app/src/main/java/com/springboot/my_boot_app/EmployeeService.java
+++ my-boot-app/src/main/java/com/springboot/my_boot_app/EmployeeService.java
@@ -1,5 +1,7 @@
 package com.springboot.my_boot_app;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,10 @@ public class EmployeeService {
 
 	public Employee addEmployee(Employee employee) {
 		return employeeRepository.save(employee);
+	}
+
+	public List<Employee> getAllEmployee() {
+		return employeeRepository.findAll();
 	}
 	
 	
--- my-boot-app/src/main/java/com/springboot/my_boot_app/controller/EmployeeController.java
+++ my-boot-app/src/main/java/com/springboot/my_boot_app/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.springboot.my_boot_app.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +20,27 @@ public class EmployeeController {
 	private EmployeeService employeeService;
 	/*
 	 * PATH: http://localhost:port/employee/add
+	 * method: POST
+	 * Description: Add customer to DB 
+	 * @Param: Employee employee
+	 * @return: Employee 
 	 * */
 	@PostMapping("/add")
 	public Employee addEmployee(@RequestBody Employee employee) { //reading the i/p
 		return employeeService.addEmployee(employee);
+	}
+	
+	/*
+	 * PATH: http://localhost:port/employee/all
+	 * method: GET 
+	 * Description: fetch all customers 
+	 * @Param: ----
+	 * @return: List<Employee> 
+	 * */
+	
+	@GetMapping("/all")
+	public List<Employee> getAllEmployee() {
+		return employeeService.getAllEmployee();
 	}
 }
 
