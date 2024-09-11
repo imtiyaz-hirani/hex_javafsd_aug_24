8f6c874200a500c5ea8b8020935dbf5aa067015f
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-09-11 10:20:52 +0530

    security-config-part-1

--- my-boot-app/src/main/java/com/springboot/my_boot_app/SecurityConfig.java
+++ my-boot-app/src/main/java/com/springboot/my_boot_app/SecurityConfig.java
@@ -2,14 +2,27 @@ package com.springboot.my_boot_app;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class AppConfig {
+public class SecurityConfig {
 
 	
 	@Bean
+	 SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests(authorize -> authorize
+				.anyRequest().authenticated()
+			)
+			.httpBasic(Customizer.withDefaults());
+		return http.build();
+	}
+	
+	@Bean 
 	PasswordEncoder getEncoder() {
 		PasswordEncoder encoder = new BCryptPasswordEncoder();
 		return encoder;
