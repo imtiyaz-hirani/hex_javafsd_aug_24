d5969bb2572cc9636df12ca0c59a4549941c94f0
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-09-26 14:33:09 +0530

    delete op added

--- my-angular-app/src/app/component/user-list/user-list.component.ts
+++ my-angular-app/src/app/component/user-list/user-list.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../service/user.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor,NgIf],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
 export class UserListComponent {
 
   users:any[] =[];
-
+  isDelete: boolean = false;
+  deleteMsg: string=''
   constructor(private userService: UserService){
     this.userService.getUsers().subscribe({
         next: (data)=>{
@@ -22,6 +23,12 @@ export class UserListComponent {
           console.log(err)
         }
     });
+  }
+
+  onDelete(id:number,name:string){
+    this.users = this.users.filter(u=>u.id != id);
+    this.isDelete = true;
+    this.deleteMsg = 'User : '+ name+ '\'s info deleted';
   }
 }
 
