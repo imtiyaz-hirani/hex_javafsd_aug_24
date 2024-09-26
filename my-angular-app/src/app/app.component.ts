fce14eb9cc78c3284de937e1a590582ad194251a
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-09-26 10:06:37 +0530

    user-screen-component-creation

--- my-angular-app/src/app/app.component.ts
+++ my-angular-app/src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
  import { NgFor, NgIf } from '@angular/common';
 import { Employee } from '../model/Employee';
 import { EmployeeService } from '../service/employee.service';
+import { NavbarComponent } from "./component/navbar/navbar.component";
+import { UserListComponent } from "./component/user-list/user-list.component";
+import { UserAddComponent } from "./component/user-add/user-add.component";
  
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NgIf],
+  imports: [RouterOutlet, NgFor, NgIf, NavbarComponent, UserListComponent, UserAddComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 }) 
